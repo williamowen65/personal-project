2f519c4a7577e885eb7081d676f50a167206787e
William Owen <william.owen.bsc@gmail.com>
2022-01-08 19:48:28 -0800

    styling

--- src/components/game/foreground/Foreground.jsx
+++ src/components/game/foreground/Foreground.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react/cjs/react.development';
+
+import Card from '../card/Card';
 
 export default function Foreground() {
+
+    const [cardData, setCardData] = useState(6)
+    const [cards, setCards] = useState([])
+
+    useEffect(() => {
+        console.log('cards: ', cards, cardData);
+        const myCards = []
+        for(let i = 0; i < cardData; i++){
+            myCards.push(<Card key={i}/>)
+        }
+        setCards(myCards)
+        console.log(myCards);
+
+    }, [cardData])
+    // console.log(cards);
+
     return (
-        <div>
-            ForeGround
+        <div className='foreground hand'>
+            {cards}
         </div>
     )
 }
